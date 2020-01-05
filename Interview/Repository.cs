099bb242fb658070bf161a56ba0b63163fad7265
Greg Murren <greg.murren@bourne-leisure.co.uk>
2020-01-05 21:28:09 +0000

    changes

---
+++ Interview/Repository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview
+{
+    public class Repository<T, I> : IRepository<T, I> where T : IStoreable<I>
+    {
+        private IList<T> repo;
+
+        public IEnumerable<T> GetAll()
+        {
+            return repo;
+        }
+
+        public T Get(I id)
+        {
+            Storable<I> storeId = new Storable<I>{Id = id};
+            return repo.FirstOrDefault(w => new Storable<I> { Id = w.Id} == storeId);
+        }
+
+        public void Delete(I id)
+        {
+            repo.RemoveAt(repo.IndexOf(Get(id)));
+        }
+
+        public void Save(T item)
+        {
+            repo.Add(item);
+        }
+    }
+}
--- InterviewTests/RepositoryTests.cs
+++ InterviewTests/RepositoryTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Interview;
 using NUnit.Framework;
 
 namespace InterviewTests
@@ -10,6 +11,20 @@ namespace InterviewTests
     [TestFixture]
     public class RepositoryTests
     {
+        [Test]
+        public void storable_Test
+        {
+            Ire
+        }
 
+        /*
+        More tests
+        */
+
+        private IRepository<T,I> CreateRepository()
+        {
+            Storable<int> id = new Storable<int>{Id = 1};
+            return new Repository<string, Storable<int>>();
+        }
     }
 }
